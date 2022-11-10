f6ad3c922b73a5d26e189d8701dae5343f4922fb
unknown <aminelufey@gmail.com>
2022-11-10 07:29:37 +0100

    fixing bugs

--- rapidapipractice/settings.py
+++ rapidapipractice/settings.py
@@ -40,11 +40,11 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-'api',
+    'api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    'django.contrib.sessions',  
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
